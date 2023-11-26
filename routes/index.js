a8124857357c5478e15f67795488e566df889301
Sumon <sumonkhan1081@gmail.com>
2023-11-26 16:14:27 +0530

    DB connected

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('../models/userModel.js');
+const postModel = require('../models/postModel.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
